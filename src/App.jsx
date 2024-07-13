c106f99cd5e81ffe0e2485dc034514cf00ed30dc
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-07-13 20:01:08 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="max-w-[268px] border-[1px] h-[213px]  mt-64 ml-[600px] rounded-xl">
+        <div className="p-[24px] flex flex-col items-center content-center">
+          <div className="bg-slate-700 w-14 h-14 rounded-full"></div>
+          <div className="pt-16px text-lg font-medium">Headline</div>
+          <p className="text-gray-600 text-center">
+            Please add your content here. Keep it short and simple. And smile{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
